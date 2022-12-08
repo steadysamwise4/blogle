6c59d48ac45ac4980351d77c77767dd7414f9c5c
steadysamwise4 <davenportsam44@gmail.com>
2022-12-08 14:56:47 -0600

    added Navigation components

--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,25 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url('https://www.bbva.com/wp-content/uploads/2017/08/innovacion-bbva-100817-1024x551.jpg');
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2" class="post-preview">
+      <PostPreview
+        id="1"
+        title="Hello there!"
+        previewText="This is my first post."
+        thumbnail="https://www.bbva.com/wp-content/uploads/2017/08/innovacion-bbva-100817-1024x551.jpg"
+      />
+      <PostPreview
+        id="2"
+        title="Hello there, the second time!"
+        previewText="This is my second post."
+        thumbnail="https://static01.nyt.com/images/2020/01/01/business/01Techfix-print/01Techfix-print-articleLarge.gif?quality=75&auto=webp&disable=upscale"
+      />
+      <PostPreview
+        id="3"
+        title="Hi!"
+        previewText="This is my third post."
+        thumbnail="https://imageio.forbes.com/specials-images/dam/imageserve/930116744/960x0.jpg?format=jpg&width=960"
+      />
+      <!-- <nuxt-link :to="'/posts/' + 2" class="post-preview">
         <article>
           <div
             class="post-thumbnail"
@@ -31,14 +35,18 @@
             <p>Preview Text 2</p>
           </div>
         </article>
-      </nuxt-link>
+      </nuxt-link> -->
     </section>
   </div>
 </template>
 
 <script>
+import PostPreview from "@/components/Posts/PostPreview";
 export default {
   name: "IndexPage",
+  components: {
+    PostPreview,
+  },
 };
 </script>
 
@@ -48,6 +56,7 @@ export default {
   position: relative;
   padding: 30px;
   box-sizing: border-box;
+  background-image: url("~assets/images/main-page-background.jpg");
   background-position: center;
   background-size: cover;
 }
@@ -80,41 +89,5 @@ export default {
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
